364983dfce5406a19e0835a9937c7b67c784ff5f
DESKTOP-NBFBMM6\knubb <Emil.N.Karlsson@gmail.com>
2019-05-15 23:30:28 +0200

    Added validation for filesize and minimum dimensions.

--- HangmanWPF/ViewModels/UploadImagSetViewModel.cs
+++ HangmanWPF/ViewModels/UploadImagSetViewModel.cs
@@ -13,8 +13,11 @@ using HangmanWPF.Models;
 
 namespace HangmanWPF.ViewModels
 {
-    public class UploadImageSetViewModel : BaseViewModel , IDialogViewModel
+    public class UploadImageSetViewModel : BaseViewModel, IDialogViewModel
     {
+        private const int ImageSizeLimitInBytes = 8388608;
+        private const int MinimumImageHeigth = 250;
+        private const int MinimumImageWidth = 250;
         private const int ImageSetSize = 9;
 
         public ObservableCollection<ImageSource> ImageCollection { get; private set; } = new ObservableCollection<ImageSource>();
@@ -40,30 +43,58 @@ namespace HangmanWPF.ViewModels
             var filedialog = new OpenFileDialog();
 
             filedialog.Filter = "Image files(*.png;*.jpg;*.bmp;*.jpeg )| *.png;*.jpg;*.bmp;*.jpeg| All files| *.*";
+            filedialog.Multiselect = false;
 
             if (filedialog.ShowDialog() == true)
             {
-                ImageSource selectedimage;
+                if (!IsImageValidForUpload(filedialog.FileName))
+                {
+                    return;
+                }
 
                 var imagedata = File.ReadAllBytes(filedialog.FileName);
 
-                selectedimage = (ImageSource)new ImageSourceConverter().ConvertFrom(imagedata);
+                ImageSource selectedimage = (ImageSource)new ImageSourceConverter().ConvertFrom(imagedata);
 
                 ImageCollection[imagenumber] = selectedimage;
-                
-            }          
+            }
         }
 
-        private void Upload()
+        private bool IsImageValidForUpload(string filepath)
         {
-            //Do some validation shenanigans
-            //TODO: Check for duplicate images (Easiest solution requires NET Core 2.1 or .NET Standard 2.1)
-            // https://stackoverflow.com/questions/43289/comparing-two-byte-arrays-in-net
-            // https://docs.microsoft.com/en-us/dotnet/api/system.memoryextensions.sequenceequal?view=netstandard-2.1
+            var sizeok = IsFileSizeUnderLimit(filepath);
+            var dimensionsok = IsImageDimensionsOverMinimumLimit(filepath);
 
-            RepositoryContainer.ImageSets.InsertImageSet(ImageDataTransformHelper.CreateDataCollectionFromImages(ImageCollection));
+            if (!sizeok)
+            {
+                MessageBox.Show($"File is too big! Please keep the size under {ImageSizeLimitInBytes / 1048576 }MB");
+            }
+            else if (!dimensionsok)
+            {
+                MessageBox.Show($"Image dimensions are too small ! Please make the image bigger than {MinimumImageWidth} x {MinimumImageHeigth}");
+            }
 
-            MessageBox.Show("Imagset uploaded");
+            return sizeok && dimensionsok;
+        }
+
+        private bool IsFileSizeUnderLimit(string filepath)
+        {
+            return new FileInfo(filepath).Length < ImageSizeLimitInBytes;
+        }
+
+        private bool IsImageDimensionsOverMinimumLimit(string filepath)
+        {
+            int width;
+            int height;
+
+            using (var stream = new FileStream(filepath, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                var bitmapFrame = BitmapFrame.Create(stream, BitmapCreateOptions.DelayCreation, BitmapCacheOption.None);
+                width = bitmapFrame.PixelWidth;
+                height = bitmapFrame.PixelHeight;
+            }
+
+            return width >= MinimumImageWidth && height >= MinimumImageHeigth;
         }
 
         public void CloseWindowAndReturnTrue(Window window)
@@ -76,6 +107,18 @@ namespace HangmanWPF.ViewModels
         public bool CanReturnResult()
         {
             return IsImageSetComplete();
+        }
+
+        private void Upload()
+        {
+            //Do some validation shenanigans
+            //TODO: Check for duplicate images (Easiest solution requires NET Core 2.1 or .NET Standard 2.1)
+            // https://stackoverflow.com/questions/43289/comparing-two-byte-arrays-in-net
+            // https://docs.microsoft.com/en-us/dotnet/api/system.memoryextensions.sequenceequal?view=netstandard-2.1
+
+            RepositoryContainer.ImageSets.InsertImageSet(ImageDataTransformHelper.CreateDataCollectionFromImages(ImageCollection));
+
+            MessageBox.Show("Imagset uploaded");
         }
 
         private bool IsImageSetComplete()
